097a175d61d51fb6e16272da1cb499de1c1543aa
tim wu <timwuuk@gmail.com>
2020-06-23 10:57:28 +0100

    website and firebase setup

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+const requestModal = document.querySelector(".new-request");
+const requestLink = document.querySelector(".add-request");
+
+requestLink.addEventListener("click", () => {
+  requestModal.classList.add("open");
+});
+
+requestModal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("new-request")) {
+    requestModal.classList.remove("open");
+  }
+});
