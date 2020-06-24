c5ec57132b6133a71e47a9bffb751d6a5fd1fc99
tim wu <timwuuk@gmail.com>
2020-06-24 01:08:35 +0100

    add auth trigers and request func

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -12,4 +14,39 @@ exports.redirectWebsite = functions.https.onRequest((req, res) => {
   res.redirect("https://www.google.com");
 });
 
-//http callable function
+//auth trigger (new user signup)
+exports.newUserSignup = functions.auth.user().onCreate(async (user) => {
+  return await admin.firestore().collection("user").doc(user.uid).set({
+    email: user.email,
+    upvotedOn: [],
+  });
+});
+
+//auth trigger (delete user)
+exports.userDeleted = functions.auth.user().onDelete(async (user) => {
+  const currentUser = await admin.firestore().collection("user").doc(user.uid);
+  currentUser.delete();
+  return { message: "user deleted" };
+});
+
+//http callable function (add request)
+exports.addRequest = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      "unauthenticated",
+      "only authenticated users can add requests"
+    );
+  }
+
+  if (data.text.length > 30) {
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "request must be no more than 30 characters long"
+    );
+  }
+
+  return await admin.firestore().collection("requests").add({
+    text: data.text,
+    upvotes: 0,
+  });
+});
--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 const requestModal = document.querySelector(".new-request");
 const requestLink = document.querySelector(".add-request");
+const requestForm = document.querySelector(".new-request form");
 
 // open request modal
 requestLink.addEventListener("click", () => {
@@ -10,5 +11,19 @@ requestLink.addEventListener("click", () => {
 requestModal.addEventListener("click", (e) => {
   if (e.target.classList.contains("new-request")) {
     requestModal.classList.remove("open");
+  }
+});
+
+//add request
+requestForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const addRequest = firebase.functions().httpsCallable("addRequest");
+    const res = await addRequest({ text: requestForm.request.value });
+    requestForm.reset();
+    requestModal.classList.remove("open");
+    requestForm.querySelector(".error").textContent = "";
+  } catch (err) {
+    requestForm.querySelector(".error").textContent = err.message;
   }
 });
